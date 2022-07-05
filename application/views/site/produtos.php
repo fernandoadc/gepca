3c69d21e05279a8d1ce5893cd5d3fdf48a913cb2
fernandoadc <tinhofernando44@gmail.com>
2022-07-05 13:41:54 -0500

    insere produtos

---
+++ application/views/site/produtos.php
@@ -0,0 +1,157 @@
+<section class="header">
+ <!-- <h2 class="title">GEPCA/UFOPA - Grupo de Estudo e Pesquisa em Computação Aplicada</h2>
+
+  <div class="value-props row">
+
+    <div class="three columns category">
+      <img class="u-max-full-width" src="<?php echo base_url('public/images/dataset.png') ?>" width="100">
+    </div>
+    <div class="nine columns category">
+      <h4>Acesse aqui os produtos do projeto, incluindo artigos, apresentações e recursos humanos..</h4>
+The products of this project include papers, presentations and human resources.<br>
+    </div>
+
+  </div>-->
+
+</section>
+
+<nav class="navbar">
+	<div class="container">
+    	<h6>
+			<b><br>Grep.Comp |</b> Grupo de Estudo<br> e Pesquisa em Computação Aplicada
+    	</h6>
+  	</div>
+	<div class="container">
+		<ul class="navbar-list">
+			<li class="navbar-item"><a class="navbar-link" href="<?php echo base_url('homepage') ?>">Home</a></li>
+			<li class="navbar-item"><a class="navbar-link" href="<?php echo base_url('team') ?>">Integrantes</a></li>
+			<li class="navbar-item"><a class="navbar-link" href="prod.html">Produtos</a></li>
+			<li class="navbar-item"><a class="navbar-link" href="<?php echo base_url('publications') ?>">Publicações</a></li>
+		</ul>
+	</div>
+</nav>
+
+<div class="docs-section" id="prod">
+    <h3 id="artigos">Produtos / Products</h3>
+    
+    <ul><h6 id="music" style="position: relative;
+    margin: 15px 0 15px 0;">Social networks, websites and blogs / Redes sociais, websites e blogs</h6>
+
+      <li><strong>Proteção intelectual de obras produzidas por sistemas baseados em inteligência artificial: 
+          <br> uma visão tecnicista sobre o tema. 2022</strong>: 
+      <p class="tab">Lobato, Fábio Manoel Franca. 
+      <br>Tema: Inteligência Artificial e Direito Autoral.
+      <br>PDF | 
+          <a target="blank" href="https://ioda.org.br/protecao-intelectual-de-obras-produzidas-por-sistemas-baseados-em-inteligencia-artificial-uma-visao-tecnicista-sobre-o-tema/">BLOG</a>	
+      </p>
+      </li>
+
+      <li><strong>What Does Social CRM Has To Do With Digital Marketing?. 2020</strong>:
+      <p class="tab">VIANA, Julio. 
+      <br>Tema: Marketing Digital e Social CRM.
+      <br>PDF | 
+          <a target="blank" href="https://scrc-leipzig.de/en/blog/single-view?tx_news_pi1%5Baction%5D=detail&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Bnews%5D=25&cHash=fde08c17ee937397b176c76ee3d6bbcd/">BLOG</a>	
+      </p>
+      </li>
+
+      <li><strong>Social CRM Na Prática: Decisões Orientadas A Dados. 2019</strong>:
+      <p class="tab">Lobato, Fábio Manoel Franca.
+      <br>Tema: Social CRM e Ciência dos Dados.
+      <br>PDF | 
+          <a target="blank" href="https://scrc-brasil.com/blog/single-view?tx_news_pi1%5Baction%5D=detail&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Bnews%5D=4&cHash=16723f7cb8f559ebfe796279493230b0">BLOG</a>	
+      </p>
+      </li>
+
+      <li><strong>Projeto BusOn do Tapajós. 2017</strong>:
+      <p class="tab">SILVA, R. J. M.
+      <br>Tema: Tecnologia e Inovação.
+      <br>SITE
+      </p>
+      </li>
+
+    </ul>
+    
+    <ul><h6 id="music" style="position: relative;
+    margin: 15px 0 15px 0;">Interviews, roundtables, programs and media commentary / Entrevistas, mesas redondas, programas e comentários na mídia</h6>
+
+
+      <li><strong>Gestão de Relacionamento com o cliente: pesquisadores da UEMA, UFPA, UFOPA e Universitat Leipzig estudam métodos computacionais inteligentes integrados à SCRM. 2022</strong>:
+      <p class="tab">JACOB JR, ANTONIO F. L.
+      <br>
+          Programa de rádio ou TV/Comentário
+      </p>
+      </li>
+
+      <li><strong>Alerta de clonagem de whatsapp. 2021</strong>:
+      <p class="tab">MANOEL FRANÇA LOBATO, FÁBIO.
+      <br>
+         <a href="https://globoplay.globo.com/v/9545414/">Programa de rádio ou TV/Comentário</a> 
+      </p>
+      </li>
+
+      <li><strong>Ufopa se consolida na produção científica no oeste do Pará. 2020.</strong>:
+      <p class="tab">BATISTA, B. B. ; MOURAO, R. H. V. ; Lobato, Fábio Manoel Franca.
+      <br>
+      <a href="https://globoplay.globo.com/v/9001280/">Programa de rádio ou TV/Comentário</a>
+      </p>
+      </li>
+
+      <li><strong>UEMA desenvolve aplicativo para mapear aglomerações no Maranhão. 2020</strong>:
+      <p class="tab">JACOB JUNIOR, A. F. L.; SILVA, R. J. ; RIBEIRO, R. P.
+      <br> 
+          <a target="blank" href="https://globoplay.globo.com/v/8543457/">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong> USP de São Carlos recebe centro de pesquisa para estudo de inteligência artificial. 2020</strong>:
+      <p class="tab">REZENDE, S. O.; OSÓRIO, Fernando Santos.
+      <br> 
+          <a target="blank" href="https://globoplay.globo.com/v/8588977/">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong> Pesquisadores da USP testam ferramentas para prever a evolução da pandemia Os cientistas elaboraram um modelo de previsão da curva de contaminação para os próximos sete dias. 2020</strong>:
+      <p class="tab">REZENDE, S. O.; Marcacini, R. M.
+      <br> 
+          <a target="blank" href="https://www.acidadeon.com/saocarlos/cotidiano/coronavirus/NOT,0,0,1494788,pesquisadores+da+usp+testam+ferramentas+para+prever+a+evolucao+da+pandemia.aspx">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong> Inteligência artificial contra a Covid-19 Especialistas da área do Brasil e do mundo interrompem projetos para se dedicar à luta contra a pandemia. 2020</strong>:
+      <p class="tab">Rezende, Solange Oliveira; MARCACINI, R. M. ; CARVALHO, A. C. P. L. F.
+      <br> 
+          <a target="blank" href="https://www.uol.com.br/tilt/noticias/redacao/2020/03/27/inteligencia-computacional-usa-noticias-para-prever-casos-de-coronavirus.html">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong> Inteligência computacional usa notícias da internet para prever casos de coronavírus. 2020.</strong>:
+      <p class="tab">Rezende, Solange Oliveira; Marcacini, R. M.
+      <br> 
+          <a target="blank" href="https://jornal.usp.br/universidade/inteligencia-computacional-usa-noticias-da-internet-para-prever-casos-de-coronavirus/">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong>Semana Acadêmica 2019: Workshop de Inovação trata de propriedade intelectual e empreendedorismo. 2019 </strong>: 
+      <p class="tab">JACOB JUNIOR, A. F. L.; MOURA, J. P. 
+      <br> 
+          <a target="blank" href="https://www.uema.br/2019/11/semana-academica-2019-workshop-de-inovacao-trata-de-propriedade-intelectual-e-empreendedorismo/">Programa de rádio ou TV/Comentário</a>	
+      </p>
+      </li>
+
+      <li><strong>Conferência Regional de Tecnologia Verde do Oeste do Pará. 2019</strong>: 
+      <p class="tab">PONTE, M.J.M.
+      <br> 
+          Programa de rádio ou TV/Comentário
+      </p>
+      </li>
+
+      <li><strong>À margem de ideologias, a realidade dos cientistas que tiveram projetos congelados no Brasil. 2019</strong>: 
+      <p class="tab">ROSSI, R. G.; MARCACINI, R. M. ; FERREIRA, F. F. ; GEROLAMO, M. C. ; COLOMBINI, E. L.
+      <br> 
+          <a href="https://brasil.elpais.com/brasil/2019/05/01/politica/1556673723_670461.html">Programa de rádio ou TV/Comentário</a>
+      </p>
+      </li>
+
+    </ul>
+    
+</div>
